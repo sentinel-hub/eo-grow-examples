88ff0454371934630f300afedba1eeecbe87cd11
Ziga Luksic <ziga.luksic@sinergise.com>
2023-01-24 13:30:23 +0100

    copy over tasks and pipelines

---
+++ mosaicking-pipeline/pipeline.py
@@ -0,0 +1,224 @@
+"""Implements a pipeline to construct features for training/prediction."""
+import logging
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
+from pydantic import Field
+
+from eolearn.core import (
+    CopyTask,
+    EONode,
+    EOWorkflow,
+    FeatureType,
+    LoadTask,
+    MergeFeatureTask,
+    OverwritePermission,
+    SaveTask,
+)
+from eolearn.features import NormalizedDifferenceIndexTask, SimpleFilterTask
+from eolearn.mask import JoinMasksTask
+
+from eogrow.core.pipeline import Pipeline
+from eogrow.core.schemas import BaseSchema
+from eogrow.tasks.features import (
+    MaxNDVIMosaickingTask,
+    MedianMosaickingTask,
+    MosaickingTask,
+    ValidDataFractionPredicate,
+    join_valid_and_cloud_masks,
+)
+from eogrow.types import Feature, FeatureSpec, PatchList, TimePeriod
+from eogrow.utils.filter import get_patches_with_missing_features
+from eogrow.utils.validators import field_validator, optional_field_validator, parse_dtype, parse_time_period
+
+LOGGER = logging.getLogger(__name__)
+
+
+class ValidityFiltering(BaseSchema):
+    cloud_mask_feature_name: Optional[str] = Field(
+        description="Name of cloud mask to enable additional filtering by cloud"
+    )
+    valid_data_feature_name: str = Field(description="Name of the valid-data mask to use for filtering.")
+
+    validity_threshold: Optional[float] = Field(
+        description="Threshold to remove frames with valid data lower than threshold"
+    )
+
+
+class MosaickingSpecifications(BaseSchema):
+    time_period: TimePeriod
+    _parse_time_period = field_validator("time_period", parse_time_period, pre=True)
+    n_mosaics: int
+
+    max_ndi_indices: Optional[Tuple[int, int]] = Field(
+        description=(
+            "When omitted uses median value mosaicking. If set, uses max NDI mosaicking for the NDI of the bands at"
+            " specified indices. For example, to use max NDVI when using all 13 bands of L1C set parameter to `[7, 3]`"
+            " (uses B08 and B04)"
+        )
+    )
+
+
+class MosaickingPipeline(Pipeline):
+    """A pipeline to calculate and prepare features for ML"""
+
+    _NDI_FEATURE = FeatureType.DATA, "_NDI_FEATURE_OF_MOSAICKING_PIPELINE"  # used by MaxNDVIMosaickingTask
+
+    class Schema(Pipeline.Schema):
+        input_folder_key: str = Field(
+            description="The storage manager key pointing to the input folder for the features pipeline."
+        )
+        output_folder_key: str = Field(
+            description="The storage manager key pointing to the output folder for the features pipeline."
+        )
+
+        bands_feature_name: str = Field(description="Name of data feature containing band data")
+
+        data_preparation: ValidityFiltering
+
+        ndis: Dict[str, Tuple[int, int]] = Field(
+            default_factory=dict,
+            description=(
+                "A dictionary of kind `{feature_name: (id1, id2)}` that specifies how to calculate the NDIs of bands "
+                "(with indices `id1` and `id2` in the bands feature) and save it under `feature_name`."
+            ),
+        )
+
+        mosaicking: MosaickingSpecifications = Field(
+            description=(
+                "Fine-tuning of mosaicking parameters. If not set, the interpolation will work on current timestamps"
+            )
+        )
+
+        dtype: Optional[np.dtype] = Field(description="The dtype under which the concatenated features should be saved")
+        _parse_dtype = optional_field_validator("dtype", parse_dtype, pre=True)
+        output_feature_name: str = Field(description="Name of output data feature encompassing bands and NDIs")
+        compress_level: int = Field(1, description="Level of compression used in saving eopatches")
+
+    config: Schema
+
+    def filter_patch_list(self, patch_list: PatchList) -> PatchList:
+        """EOPatches are filtered according to existence of specified output features"""
+
+        filtered_patch_list = get_patches_with_missing_features(
+            self.storage.filesystem,
+            self.storage.get_folder(self.config.output_folder_key),
+            patch_list,
+            self._get_output_features(),
+        )
+
+        return filtered_patch_list
+
+    def _get_output_features(self) -> List[FeatureSpec]:
+        """Lists all features that are to be saved upon the pipeline completion"""
+        return [(FeatureType.DATA, self.config.output_feature_name), FeatureType.BBOX, FeatureType.TIMESTAMP]
+
+    def _get_bands_feature(self) -> Feature:
+        return FeatureType.DATA, self.config.bands_feature_name
+
+    def _get_valid_data_feature(self) -> Feature:
+        return FeatureType.MASK, self.config.data_preparation.valid_data_feature_name
+
+    def build_workflow(self) -> EOWorkflow:
+        """
+        Creates a workflow:
+        1. Loads and prepares a 'bands_feature' and 'valid_data_feature'
+        2. Temporally regularizes bands and NDIs
+        3. Calculates NDIs based on 'bands_feature'
+        4. Applies post-processing, which prepares all output features
+        5. Saves all relevant features (specified in _get_output_features)
+        """
+        preparation_node = self.get_data_preparation_node()
+        regularization_node = self.get_temporal_regularization_node(preparation_node)
+        ndi_node = self.get_ndi_node(regularization_node)
+        postprocessing_node = self.get_postprocessing_node(ndi_node)
+
+        save_task = SaveTask(
+            self.storage.get_folder(self.config.output_folder_key),
+            filesystem=self.storage.filesystem,
+            features=self._get_output_features(),
+            compress_level=self.config.compress_level,
+            overwrite_permission=OverwritePermission.OVERWRITE_FEATURES,
+        )
+        save_node = EONode(save_task, inputs=[postprocessing_node])
+
+        return EOWorkflow.from_endnodes(save_node)
+
+    def get_data_preparation_node(self) -> EONode:
+        """Nodes that load, filter, and prepare a feature containing all bands
+
+        :return: A node with preparation tasks and feature for masking invalid data
+        """
+        filtering_config = self.config.data_preparation
+
+        load_task = LoadTask(
+            self.storage.get_folder(self.config.input_folder_key),
+            filesystem=self.storage.filesystem,
+            lazy_loading=True,
+        )
+        end_node = EONode(load_task)
+
+        if filtering_config.cloud_mask_feature_name:
+            zip_masks_task = JoinMasksTask(
+                [self._get_valid_data_feature(), (FeatureType.MASK, filtering_config.cloud_mask_feature_name)],
+                self._get_valid_data_feature(),
+                join_operation=join_valid_and_cloud_masks,
+            )
+            end_node = EONode(zip_masks_task, inputs=[end_node], name="Combine validity mask and cloud mask")
+
+        if filtering_config.validity_threshold is not None:
+            filter_func = ValidDataFractionPredicate(filtering_config.validity_threshold)
+            filter_task = SimpleFilterTask(self._get_valid_data_feature(), filter_func)
+            end_node = EONode(filter_task, inputs=[end_node])
+
+        if self.config.mosaicking.max_ndi_indices:
+            ndi_task = NormalizedDifferenceIndexTask(
+                self._get_bands_feature(), self._NDI_FEATURE, self.config.mosaicking.max_ndi_indices
+            )
+            end_node = EONode(ndi_task, inputs=[end_node])
+
+        return end_node
+
+    def get_temporal_regularization_node(self, previous_node: EONode) -> EONode:
+        start_date, end_date = self.config.mosaicking.time_period
+
+        mosaicking_task: MosaickingTask
+        if self.config.mosaicking.max_ndi_indices:
+            mosaicking_task = MaxNDVIMosaickingTask(
+                self._get_bands_feature(),
+                (start_date, end_date, self.config.mosaicking.n_mosaics),
+                self._NDI_FEATURE,
+                self._get_valid_data_feature(),
+            )
+        else:
+            mosaicking_task = MedianMosaickingTask(
+                self._get_bands_feature(),
+                (start_date, end_date, self.config.mosaicking.n_mosaics),
+                self._get_valid_data_feature(),
+            )
+
+        mosaicking_node = EONode(mosaicking_task, inputs=[previous_node])
+        return EONode(
+            CopyTask(features=[self._get_bands_feature(), FeatureType.BBOX, FeatureType.TIMESTAMP]),
+            inputs=[mosaicking_node],
+            name="Remove non-mosaicked features",
+        )
+
+    def get_ndi_node(self, previous_node: EONode) -> EONode:
+        """Builds a node for constructing Normalized Difference Indices"""
+
+        for name, (id1, id2) in self.config.ndis.items():
+            ndi_task = NormalizedDifferenceIndexTask(self._get_bands_feature(), (FeatureType.DATA, name), [id1, id2])
+            previous_node = EONode(ndi_task, inputs=[previous_node])
+
+        return previous_node
+
+    def get_postprocessing_node(self, previous_node: EONode) -> EONode:
+        """Tasks performed after temporal regularization. Should also prepare features for the saving step"""
+        ndi_features = [(FeatureType.DATA, name) for name in self.config.ndis]
+        merge_task = MergeFeatureTask(
+            [self._get_bands_feature(), *ndi_features],
+            (FeatureType.DATA, self.config.output_feature_name),
+            dtype=self.config.dtype,
+        )
+        return EONode(merge_task, inputs=[previous_node])
---
+++ mosaicking-pipeline/tasks.py
@@ -0,0 +1,187 @@
+"""Implements tasks needed for calculating features in FeaturesPipeline."""
+import abc
+from datetime import date, datetime, time, timedelta
+from typing import List, Optional, Sequence, Tuple, Union
+
+import numpy as np
+
+from eolearn.core import EOPatch, EOTask, FeatureType
+
+from eogrow.types import Feature
+
+
+class MosaickingTask(EOTask, metaclass=abc.ABCMeta):
+    """Base class for mosaicking images given an interval of edge dates."""
+
+    def __init__(
+        self,
+        feature: Feature,
+        dates: Union[List[date], Tuple[date, date, int]],
+        valid_mask: Optional[Feature] = None,
+        ndvi_feature: Optional[Feature] = None,
+    ):
+        self.parsed_feature = self.parse_renamed_feature(feature, allowed_feature_types={FeatureType.DATA})
+        self.valid_mask_type, self.valid_mask_name = None, None
+        if valid_mask is not None:
+            self.valid_mask_type, self.valid_mask_name = self.parse_feature(
+                valid_mask, allowed_feature_types={FeatureType.MASK}
+            )
+        self.ndvi_feature_type, self.ndvi_feature_name = None, None
+        if ndvi_feature is not None:
+            self.ndvi_feature_type, self.ndvi_feature_name = self.parse_feature(
+                ndvi_feature, allowed_feature_types={FeatureType.DATA}
+            )
+        self.dates = self._get_dates(dates)
+
+    def _get_dates(self, dates: Union[List[date], Tuple[date, date, int]]) -> np.ndarray:
+        """Set dates either from list of dates or a tuple (start_date, end_date, n_mosaics)"""
+        if all(isinstance(d, (date, datetime)) for d in dates):
+            return np.array(dates)
+        if len(dates) == 3 and isinstance(dates[-1], int):
+            return self._get_date_edges(*dates)
+        raise ValueError(
+            "dates parameter can be either a list of date(time)s or a tuple "
+            "(start_date, end_date, n_mosaics) for equidistant intervals between start and end date."
+        )
+
+    @staticmethod
+    def _get_date_edges(start_date: date, end_date: date, parts: int) -> np.ndarray:
+        """Help function to get dates of year split into equal parts
+
+        :param start_date: first date of time interval
+        :param end_date: last date of time interval
+        :param parts: Number of parts to split the year into
+        :return: numpy array of dates that split the time interval into equal parts
+        """
+        start = datetime.combine(start_date, time.min)
+        end = datetime.combine(end_date, time.min) + timedelta(days=1)
+        diff = (end - start) / parts
+        edges = [start + diff * i for i in range(parts)]
+        edges.append(end)
+        return np.array(edges)
+
+    def _find_time_indices(self, timestamps: Sequence[date], index: int) -> Tuple[np.ndarray, ...]:
+        """Compute indices of images to use for mosaicking"""
+        if index == 1:
+            array = np.where((np.array(timestamps) <= self.dates[index]))
+        elif index == len(self.dates) - 1:
+            array = np.where((np.array(timestamps) > self.dates[index - 1]))
+        else:
+            array = np.where(
+                (np.array(timestamps) > self.dates[index - 1]) & (np.array(timestamps) <= self.dates[index])
+            )
+        return array
+
+    def compute_mosaic_dates(self) -> np.ndarray:
+        """Compute dates of corresponding mosaics"""
+        # calculate centers of date edges
+        delta = self.dates[1:] - self.dates[:-1]
+        return self.dates[:-1] + delta / 2
+
+    @abc.abstractmethod
+    def _compute_single_mosaic(self, eopatch: EOPatch, idate: int) -> np.ndarray:
+        """Compute single mosaic given index of edge date"""
+
+    def compute_mosaic(self, eopatch: EOPatch) -> np.ndarray:
+        """Computes mosaic"""
+        return np.array([self._compute_single_mosaic(eopatch, idate) for idate in range(1, len(self.dates))])
+
+    def execute(self, eopatch: EOPatch) -> EOPatch:
+        """Compute mosaic for given dates"""
+        feature_type, _, new_feature_name = self.parsed_feature
+
+        eopatch.timestamp = [ts.replace(tzinfo=None) for ts in eopatch.timestamp]
+        eopatch[(feature_type, new_feature_name)] = self.compute_mosaic(eopatch)
+
+        eopatch.timestamp = list(self.compute_mosaic_dates())
+
+        return eopatch
+
+
+class MaxNDVIMosaickingTask(MosaickingTask):
+    """Task to create mosaics of data based on the max NDVI value between provided dates."""
+
+    def __init__(
+        self,
+        feature: Feature,
+        dates: Union[List[date], Tuple[date, date, int]],
+        ndvi_feature: Feature,
+        valid_mask: Optional[Feature] = None,
+    ):
+        super().__init__(feature, dates, ndvi_feature=ndvi_feature, valid_mask=valid_mask)
+
+    def _compute_single_mosaic(self, eopatch: EOPatch, idate: int) -> np.ndarray:
+        """Compute single mosaic using values of the max NDVI"""
+        array = self._find_time_indices(eopatch.timestamp, idate)
+        feature_type, feature_name, _ = self.parsed_feature
+        feat_values = eopatch[(feature_type, feature_name)][array].astype(np.float32)
+        ndvi_values = eopatch[(self.ndvi_feature_type, self.ndvi_feature_name)][array]  # type: ignore[index]
+        valid_mask = (
+            eopatch[self.valid_mask_type][self.valid_mask_name][array]
+            if self.valid_mask_type is not None
+            else np.ones(feat_values.shape, dtype=bool)
+        ).astype(bool)
+
+        ndvi_values[~valid_mask] = np.nan
+        feat_values[~np.broadcast_to(valid_mask, feat_values.shape)] = np.nan
+
+        mask_nan_slices = np.all(np.isnan(ndvi_values), axis=0, keepdims=True)
+        ndvi_values[np.broadcast_to(mask_nan_slices, ndvi_values.shape)] = -999
+        feat_values[np.broadcast_to(mask_nan_slices, feat_values.shape)] = -999
+
+        timeframes, height, width, depth = feat_values.shape
+
+        if timeframes == 0:
+            mosaic = np.full((height, width, depth), np.nan)
+        else:
+            if timeframes == 1:
+                mosaic = feat_values[0]
+            else:
+                indices = np.nanargmax(ndvi_values, axis=0).squeeze(axis=-1)
+                ixgrid: Tuple[np.ndarray, ...] = np.ix_(np.arange(timeframes), np.arange(height), np.arange(width))
+                mosaic = feat_values[indices, ixgrid[1], ixgrid[2], :].squeeze(axis=0)
+
+            mosaic[np.broadcast_to(mask_nan_slices[0], mosaic.shape)] = np.nan
+        return mosaic
+
+
+class MedianMosaickingTask(MosaickingTask):
+    """Task to create mosaics of data based on the median value between provided dates."""
+
+    def __init__(
+        self,
+        feature: Feature,
+        dates: Union[List[date], Tuple[date, date, int]],
+        valid_mask: Optional[Feature] = None,
+    ):
+        super().__init__(feature, dates, valid_mask=valid_mask)
+
+    def _compute_single_mosaic(self, eopatch: EOPatch, idate: int) -> np.ndarray:
+        """Compute single mosaic using the median of values"""
+        array = self._find_time_indices(eopatch.timestamp, idate)
+        feature_type, feature_name, _ = self.parsed_feature
+
+        feat_values = eopatch[(feature_type, feature_name)][array].astype(np.float32)
+        valid_mask = (
+            eopatch[(self.valid_mask_type, self.valid_mask_name)][array]
+            if self.valid_mask_type is not None
+            else np.ones(feat_values.shape, dtype=bool)
+        )
+
+        feat_values[~np.broadcast_to(valid_mask, feat_values.shape)] = np.nan
+
+        mask_nan_slices = np.all(np.isnan(feat_values), axis=0, keepdims=True)
+        feat_values[np.broadcast_to(mask_nan_slices, feat_values.shape)] = -999
+        timeframes, height, width, depth = feat_values.shape
+
+        if timeframes == 0:
+            mosaic = np.full((height, width, depth), np.nan)
+        else:
+            if timeframes == 1:
+                mosaic = feat_values[0]
+            else:
+                mosaic = np.nanmedian(feat_values, axis=0)
+
+            mosaic[np.broadcast_to(mask_nan_slices[0], mosaic.shape)] = np.nan
+
+        return mosaic
